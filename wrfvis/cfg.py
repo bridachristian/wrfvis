af861f54a63f82135cd6c16be1cd3445fbd95e06
Christian Brida <bridachristian@gmail.com>
2023-12-09 19:21:00 +0100

    test gitignore

--- wrfvis/cfg.py
+++ wrfvis/cfg.py
@@ -1,7 +1,7 @@
 """ Configuration module containing settings and constants. """
 import os
 
-wrfout = 'mydir/WRF_output_project.nc'
+wrfout = 'test/WRF_output_project.nc'
 
 # location of data directory
 pkgdir = os.path.dirname(__file__)
